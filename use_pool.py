24b9a7968c2d70f0ada5f815972eb4f0df6d0e0a
Elizaveta Dakhova <elizaveta.dakhova@bostongene.com>
2018-08-02 13:40:22 +0300

    example of using pool with time comparison

--- use_pool.py
+++ use_pool.py
@@ -8,8 +8,10 @@ import argparse
 # parse args
 parser = argparse.ArgumentParser()
 parser.add_argument('-n', help='number in range function')
+parser.add_argument('-cores', help='number of cores', default='4')
 p = parser.parse_args()
 n = int(p.n)
+cores = int(p.cores)
 
 
 def sqrt_of_module(x):
@@ -18,7 +20,7 @@ def sqrt_of_module(x):
 
 # with using multiprocessing
 start_time = time.time()
-pool = mp.Pool(processes=4)
+pool = mp.Pool(processes=cores)
 results = pool.map(sqrt_of_module, range(n))
 # print(results)
 print("--- using multiprocessing it lasts %s seconds ---" % (time.time() - start_time))
